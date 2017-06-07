5cd4d28dc1bb4ef457bba2500da703ec983abddf
yan <yanbobersky@gmail.com>
2017-06-07 23:50:54 +0800

    install pymysql.

--- QiuBaiSpider/pymysqldb_manager.py
+++ QiuBaiSpider/pymysqldb_manager.py
@@ -4,27 +4,39 @@ from QiuBaiSpider.tools import Tools
 
 
 class DbManager(object):
+    sql_create_table = """CREATE TABLE IF NOT EXISTS QiuShiBaiKe (
+             name  CHAR(512) NOT NULL,
+             content  CHAR(4096))"""
+
     def __init__(self):
         self.db = None
         self.cursor = None
 
     def connect(self):
         try:
-            self.db = pymysql.connect("localhost", "testuser", "test123", "TESTDB")
-            self.db.set_charset('utf8')
+            self.db = pymysql.connect("localhost", "yanbober", "19900322", "database_yan_php")
+            #self.db.set_charset('utf8')
             self.cursor = self.db.cursor()
-        except:
-            pass
+            self.cursor.execute(DbManager.sql_create_table)
+        except Exception as e:
+            print(str(e))
 
     def close(self):
         try:
             if self.db is not None:
                 self.db.close()
+            if self.db is not None:
+                self.cursor.close()
         except BaseException as e:
             Tools.log("#ERROR# DbManager close failed."+str(e))
 
     def insertDict(self, dict_data=None):
         if dict_data is None:
             return False
-        pass
+        sql = "INSERT INTO QiuShiBaiKe(name, content) VALUES ("+dict_data["name"]+","+dict_data["content"]+")"
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
         return True
--- QiuBaiSpider/spider_main.py
+++ QiuBaiSpider/spider_main.py
@@ -11,11 +11,16 @@ class Main(object):
         self.db_manager = DbManager()
 
     def run(self):
+        self.db_manager.connect();
         for index in range(10):
             self._page_run(index)
 
+        self.db_manager.close();
+
     def _page_run(self, page):
         page_dict_items = PageItems(page).get_page_dict_items()
+        for dict_item in page_dict_items:
+            self.db_manager.insertDict(dict_item)
         pass
 
 if __name__ == "__main__":
