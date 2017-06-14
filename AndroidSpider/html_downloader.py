b4594f694099c29e5feef34c39f5665ce9131848
yan <yanbobersky@gmail.com>
2017-06-14 11:03:45 +0800

    downloader pop up!

--- AndroidSpider/html_downloader.py
+++ AndroidSpider/html_downloader.py
@@ -1,11 +1,23 @@
-import urllib
+from urllib import request, error
+from urllib.parse import urlparse
 
 
 class HtmlDownLoader(object):
-    def download(self, url):
+    def download(self, url, retry_count=3, headers=None, proxy=None):
         if url is None:
             return None
-        response = urllib.request.urlopen(url)
-        if response.getcode() != 200:
-            return None
-        return response.read()
+        try:
+            req = request.Request(url, headers=headers)
+            opener = request.build_opener()
+            if proxy:
+                proxies = {urlparse(url).scheme: proxy}
+                opener.add_handler(request.ProxyHandler(proxies))
+            content = request.urlopen(req).read()
+        except error.URLError as e:
+            print('HtmlDownLoader download error:', e.reason)
+            content = None
+            if retry_count > 0:
+                if hasattr(e, 'code') and 500 <= e.code < 600:
+                    #说明是 HTTPError 错误且 HTTP CODE 为 5XX 范围说明是服务器错误，可以尝试再次下载
+                    return self.download(url, retry_count-1)
+        return content
--- AndroidSpider/spider_main.py
+++ AndroidSpider/spider_main.py
@@ -20,7 +20,10 @@ class SpiderMain(object):
             try:
                 new_url = self.urls.get_new_url()
                 print("craw %d : %s" % (count, new_url))
-                html_content = self.downloader.download(new_url)
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36"
+                }
+                html_content = self.downloader.download(new_url, retry_count=2, headers=headers)
                 new_urls, new_data = self.parser.parse(new_url, html_content, "utf-8")
                 self.urls.add_new_urls(new_urls)
                 self.out_put.collect_data(new_data)
