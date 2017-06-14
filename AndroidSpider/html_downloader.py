90daeb9b940a6bfd039f3ac8d21f53ff72dfc73d
yan <yanbobersky@gmail.com>
2017-06-14 11:19:49 +0800

    downloader pop up!

--- AndroidSpider/html_downloader.py
+++ AndroidSpider/html_downloader.py
@@ -1,18 +1,21 @@
+from http import cookiejar
 from urllib import request, error
 from urllib.parse import urlparse
 
 
 class HtmlDownLoader(object):
-    def download(self, url, retry_count=3, headers=None, proxy=None):
+    def download(self, url, retry_count=3, headers=None, proxy=None, data=None):
         if url is None:
             return None
         try:
-            req = request.Request(url, headers=headers)
+            req = request.Request(url, headers=headers, data=data)
+            cookie = cookiejar.CookieJar()
+            cookie_process = request.HTTPCookieProcessor(cookie)
             opener = request.build_opener()
             if proxy:
                 proxies = {urlparse(url).scheme: proxy}
                 opener.add_handler(request.ProxyHandler(proxies))
-            content = request.urlopen(req).read()
+            content = opener.open(req).read()
         except error.URLError as e:
             print('HtmlDownLoader download error:', e.reason)
             content = None
