df2e1110537e4f50842537715080eb181dad5d15
gongsiwin10 <lhuibin@gmail.com>
2018-07-16 17:32:35 +0800

    ''

--- AndroidSpider/bs_test.py
+++ AndroidSpider/bs_test.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-m=(k/8)//8
+
+import requests
+
+params = {'id': 'ctl00', }
+r = requests.post("https://etherscan.io/txsInternal?a=0x7751278c8b6b224946d065776fc67c81a5202958&&valid=true&p=1", data=params)
+print(r.text('a'))
