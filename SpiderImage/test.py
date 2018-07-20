3ac04ba07e9f70407f271baba9b1a134afd1bfd6
gongsiwin10 <lhuibin@gmail.com>
2018-07-20 17:31:17 +0800

    'test'

--- SpiderImage/test.py
+++ SpiderImage/test.py
@@ -1,10 +1,4 @@
-# -*- coding:UTF-8 -*-
+# -*- coding:utf-8 -*-
 
 from urllib import request
-import chardet
-
-if __name__ == '__main__':
-	respose=request.urlopen('http://www.baidu.com')
-	html=respose.read()
-	charset=chardet.detect(html)
-	print(charset)
+from urllib import parse
